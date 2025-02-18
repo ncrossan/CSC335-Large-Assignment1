6ef350bdd3eb7c1a27f2c1a4afdc95402ddc5776
ncrossan <minathaniel7@gmail.com>
2025-02-17 19:38:39 -0700

    Add album directory with input text files

--- Large_Assignment_1/src/MusicStore.java
+++ Large_Assignment_1/src/MusicStore.java
@@ -1,4 +1,21 @@
+/*
+ * Authors: Nathaniel Crossan, Andy Zhang
+ * CSC 335 - Large Assignment 1
+ * Description: The class MusicStore 
+ * */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class MusicStore {
-
+	private HashMap<String, String> musicLibrary; // change to song and album objects
+	
+	public MusicStore() {
+		// load hashmap with txt files when library instance is created
+		
+	}
 }
