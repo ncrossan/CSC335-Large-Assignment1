3427f10f5b848cbc657679d5c936b54104ced10a
ncrossan <minathaniel7@gmail.com>
2025-02-23 11:12:48 -0700

    wrote code for Song class; made immutable

--- Large_Assignment_1/src/model/Song.java
+++ Large_Assignment_1/src/model/Song.java
@@ -1,5 +1,23 @@
 package model;
 
 public class Song {
-
+	private String title;
+	private String artist;
+	
+	public Song(String title, String artist) {
+		this.title = title;
+		this.artist = artist;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String toString() {
+		return title + " by " + artist;
+	}
 }
