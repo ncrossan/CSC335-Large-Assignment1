4f29d0af7b013b51a38935037cbad3d6fcedc58f
ncrossan <minathaniel7@gmail.com>
2025-02-23 12:23:01 -0700

    Removed import statements from MusicStore

--- Large_Assignment_1/src/model/MusicStore.java
+++ Large_Assignment_1/src/model/MusicStore.java
@@ -6,18 +6,16 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class MusicStore {
-	private HashMap<String, String> musicLibrary; // change to song and album objects
+	
 	
 	public MusicStore() {
-		// load hashmap with txt files when library instance is created
+		
 		
 	}
 }
