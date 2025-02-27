4b8f99378988a86593b0fc43c7665786ba3ad659
ncrossan <minathaniel7@gmail.com>
2025-02-27 16:36:05 -0700

    edited a typo

--- Large_Assignment_1/src/model/LibraryModel.java
+++ Large_Assignment_1/src/model/LibraryModel.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class LibraryModel {
 	private MusicStore musicStore;
-	private ArrayList<Playlist> playlists;
-	private Playlist favorites;
+	private ArrayList<PlayList> playlists;
+	private PlayList favorites;
 	
 	
 }
