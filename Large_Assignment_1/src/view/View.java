6121f81b47442fecc8f5e154648948663403e76a
xeryux <57974809+xeryux@users.noreply.github.com>
2025-02-27 13:33:38 -0700

    Update View.java

--- Large_Assignment_1/src/view/View.java
+++ Large_Assignment_1/src/view/View.java
@@ -1,1 +1,25 @@
+package view;
 
+import java.util.Scanner;
+
+public class View {
+	
+	
+	// to be implemented.. prompt user for input
+	public void prompUser() {
+		Scanner keyboardScanner = new Scanner(System.in);
+		System.out.println("command?");
+		String command = keyboardScanner.nextLine();
+		
+		// user can type exit to terminate the program.
+		if (command.toLowerCase().equals("exit")) System.exit(0);
+		
+		
+		keyboardScanner.close();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
