404fefbfd546013c54f2ff84899e2091b1bbff60
xeryux <57974809+xeryux@users.noreply.github.com>
2025-02-27 19:08:56 -0700

    Update PlayListTest.java

--- Large_Assignment_1/src/model/PlayListTest.java
+++ Large_Assignment_1/src/model/PlayListTest.java
@@ -1,1 +1,35 @@
+package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.jupiter.api.Test;
+
+class PlayListTest {
+
+	@Test
+	void testAddSong() throws FileNotFoundException {
+		MusicStore musicStore = new MusicStore();
+		PlayList playlist = new PlayList("test");
+		playlist.addSong(musicStore.getSong("My Heart Is Full", "Norah Jones"));
+		assertEquals(playlist.toString(), "test\nMy Heart Is Full by Norah Jones\n");
+	}
+	
+	@Test
+	void testRemoveSong() throws FileNotFoundException {
+		MusicStore musicStore = new MusicStore();
+		PlayList playlist = new PlayList("test");
+		playlist.addSong(musicStore.getSong("My Heart Is Full", "Norah Jones"));
+		playlist.addSong(musicStore.getSong("Begin Again", "Norah Jones"));
+		playlist.removeSong(musicStore.getSong("Begin Again", "Norah Jones"));
+		assertEquals(playlist.toString(), "test\nMy Heart Is Full by Norah Jones\n");
+
+	}
+	
+	@Test
+	void testGetName() throws FileNotFoundException {
+		PlayList playlist = new PlayList("test");
+		assertEquals(playlist.getName(), "test");
+	}
+}
